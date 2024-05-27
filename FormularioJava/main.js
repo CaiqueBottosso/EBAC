db620333dd09fa7a265f10ade0caede8223d2922
CaiqueBottosso <caiquecoleto@gmail.com>
2024-05-27 11:47:56 +0100

    .

--- FormularioJava/main.js
+++ FormularioJava/main.js
@@ -1,7 +1,29 @@
 const form = document.getElementById("form-deposito");
 
+function validaNome(nomeCompleto) {
+  const nomeComoArrey = nomeCompleto.split(" ");
+  return nomeComoArrey.length >= 2;
+}
+
 form.addEventListener("submit", function (e) {
+  let fomrEValido = false;
   e.preventDefault();
+
+  const nomeBeneficiario = document.getElementById("nome-beneficiario");
+  const numeroContaBeneficiario = document.getElementById("numero-conta");
+  const valoDeposito = document.getElementById("valor-deposito");
+  const mensagemSucesso = `Montante de: ${valorDeposito.value} depositado para o cliente: ${nomeBeneficiario.value} - conta ${numeroContaBeneficiario.value}`;
+
+  fomrEValido = validaNome(nomeBeneficiario.value);
+  if (fomrEValido) {
+    alert(mensagemSucesso);
+
+    nomeBeneficiario.value = "";
+    numeroContaBeneficiario.value = "";
+    valoDeposito.value = "";
+  } else {
+    alert("O nome nao esta completo");
+  }
 });
 
 console.log(form);
